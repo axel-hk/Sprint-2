23584ce42a2f54bfc5b54aacd0a853e6ca0e7ae9
Alexander Annenkov <55015984+axel-hk@users.noreply.github.com>
2021-08-29 19:13:24 +0300

    Add files via upload
    Исправил код в соответствии с замечаниями

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -6,12 +6,13 @@ class ProblemD {
         for(i in 1..t) {
             val n = readInt()
             val arr = readLongArray(n)
-            val SortedArr = arr.sortedDescending()
-            val maxProfit = ArrayList<Long>()
-            for(j in SortedArr.indices){
-                maxProfit.add(SortedArr[j]*(j+1))
+            val sortedArr = arr.sortedDescending()
+            var maxProfit: Long = 0
+            for(j in sortedArr.indices) {
+                if (sortedArr[j] * (j + 1) > maxProfit)
+                    maxProfit = sortedArr[j] * (j + 1)
             }
-            println(maxProfit.maxOrNull())
+            println(maxProfit)
         }
 
     }
