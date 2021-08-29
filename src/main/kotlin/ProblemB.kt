29a63fccb002d187c2669494e91a857448bd11e1
Alexander Annenkov <55015984+axel-hk@users.noreply.github.com>
2021-08-29 19:25:53 +0300

    Исправил задание

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -12,38 +12,14 @@ class ProblemB {
             val arr: IntArray = readIntArray(2)
             //Длина строки
             val len = arr[0]
+            //Количество букв
             val uniq = arr[1]
-            val min: Int = len / uniq
-            //Позиции букв латинского алфавита а - 0, b - 1, с -2...
-            val index = IntArray(uniq)
-            while (str.length < len){
-                //Ascii код буквы
-                var character: Int = Random.nextInt(97,97+uniq)
-
-                     //Если есть буква, которая ещё не достигла предела повторений
-                    if((min > index[character - 97])) {
-                        str += character.toChar()
-                        index[character - 97]++
-                    }
-                    //Если есть неиспользованные буквы
-                    else if((index.contains(0))){
-                        val lastVal = index.lastIndexOf(0)
-                        character = lastVal + 97
-                        str += character.toChar()
-                        index[character - 97]++
-
-                    }
-                    //Если все буквы использовались и предел повторений достигнут
-                    else if(str.length == len-1){
-                        character = Random.nextInt(97, 97 + uniq)
-                        str += character.toChar()
-                    }
-                }
-
+            for (j in 0 until len) {
+                str += Char(97 + j % uniq)
+            }
 
             println(str)
         }
-
 
     }
 
